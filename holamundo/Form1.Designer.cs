98ae84d5a782df57062d34f2abe8e8d999964454
cristian morales <christianjack01@hotmail.com>
2019-08-25 12:47:01 -0500

    se añaden calculos con textbox

--- holamundo/Form1.Designer.cs
+++ holamundo/Form1.Designer.cs
@@ -32,6 +32,15 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtnombre = new System.Windows.Forms.TextBox();
             this.btnpresioname = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtnumero1 = new System.Windows.Forms.TextBox();
+            this.txtnumero2 = new System.Windows.Forms.TextBox();
+            this.btnmultipliacar = new System.Windows.Forms.Button();
+            this.btndividir = new System.Windows.Forms.Button();
+            this.btnsumar = new System.Windows.Forms.Button();
+            this.btnrestar = new System.Windows.Forms.Button();
+            this.lbltotal = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -57,24 +66,114 @@
             // 
             this.txtnombre.Location = new System.Drawing.Point(240, 115);
             this.txtnombre.Name = "txtnombre";
-            this.txtnombre.Size = new System.Drawing.Size(100, 31);
+            this.txtnombre.Size = new System.Drawing.Size(145, 31);
             this.txtnombre.TabIndex = 2;
             // 
             // btnpresioname
             // 
-            this.btnpresioname.Location = new System.Drawing.Point(346, 115);
+            this.btnpresioname.Location = new System.Drawing.Point(394, 115);
             this.btnpresioname.Name = "btnpresioname";
-            this.btnpresioname.Size = new System.Drawing.Size(207, 38);
+            this.btnpresioname.Size = new System.Drawing.Size(207, 31);
             this.btnpresioname.TabIndex = 3;
             this.btnpresioname.Text = "presioname";
             this.btnpresioname.UseVisualStyleBackColor = true;
             this.btnpresioname.Click += new System.EventHandler(this.btnpresioname_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ingresa u numero:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(210, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "ingresa otro numero:";
+            // 
+            // txtnumero1
+            // 
+            this.txtnumero1.Location = new System.Drawing.Point(240, 221);
+            this.txtnumero1.Name = "txtnumero1";
+            this.txtnumero1.Size = new System.Drawing.Size(145, 31);
+            this.txtnumero1.TabIndex = 5;
+            // 
+            // txtnumero2
+            // 
+            this.txtnumero2.Location = new System.Drawing.Point(240, 266);
+            this.txtnumero2.Name = "txtnumero2";
+            this.txtnumero2.Size = new System.Drawing.Size(145, 31);
+            this.txtnumero2.TabIndex = 5;
+            // 
+            // btnmultipliacar
+            // 
+            this.btnmultipliacar.Location = new System.Drawing.Point(475, 263);
+            this.btnmultipliacar.Name = "btnmultipliacar";
+            this.btnmultipliacar.Size = new System.Drawing.Size(75, 34);
+            this.btnmultipliacar.TabIndex = 6;
+            this.btnmultipliacar.Text = "x";
+            this.btnmultipliacar.UseVisualStyleBackColor = true;
+            this.btnmultipliacar.Click += new System.EventHandler(this.btnmultipliacar_Click);
+            // 
+            // btndividir
+            // 
+            this.btndividir.Location = new System.Drawing.Point(556, 264);
+            this.btndividir.Name = "btndividir";
+            this.btndividir.Size = new System.Drawing.Size(75, 34);
+            this.btndividir.TabIndex = 6;
+            this.btndividir.Text = "/";
+            this.btndividir.UseVisualStyleBackColor = true;
+            this.btndividir.Click += new System.EventHandler(this.btndividir_Click);
+            // 
+            // btnsumar
+            // 
+            this.btnsumar.Location = new System.Drawing.Point(475, 217);
+            this.btnsumar.Name = "btnsumar";
+            this.btnsumar.Size = new System.Drawing.Size(75, 35);
+            this.btnsumar.TabIndex = 6;
+            this.btnsumar.Text = "+";
+            this.btnsumar.UseVisualStyleBackColor = true;
+            this.btnsumar.Click += new System.EventHandler(this.btnsumar_Click);
+            // 
+            // btnrestar
+            // 
+            this.btnrestar.Location = new System.Drawing.Point(556, 217);
+            this.btnrestar.Name = "btnrestar";
+            this.btnrestar.Size = new System.Drawing.Size(75, 35);
+            this.btnrestar.TabIndex = 6;
+            this.btnrestar.Text = "-";
+            this.btnrestar.UseVisualStyleBackColor = true;
+            this.btnrestar.Click += new System.EventHandler(this.btnrestar_Click);
+            // 
+            // lbltotal
+            // 
+            this.lbltotal.AutoSize = true;
+            this.lbltotal.Location = new System.Drawing.Point(102, 359);
+            this.lbltotal.Name = "lbltotal";
+            this.lbltotal.Size = new System.Drawing.Size(53, 25);
+            this.lbltotal.TabIndex = 7;
+            this.lbltotal.Text = "total";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(849, 865);
+            this.Controls.Add(this.lbltotal);
+            this.Controls.Add(this.btnrestar);
+            this.Controls.Add(this.btnsumar);
+            this.Controls.Add(this.btndividir);
+            this.Controls.Add(this.btnmultipliacar);
+            this.Controls.Add(this.txtnumero2);
+            this.Controls.Add(this.txtnumero1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnpresioname);
             this.Controls.Add(this.txtnombre);
             this.Controls.Add(this.label2);
@@ -83,6 +182,7 @@
             this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,6 +194,15 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtnombre;
         private System.Windows.Forms.Button btnpresioname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtnumero1;
+        private System.Windows.Forms.TextBox txtnumero2;
+        private System.Windows.Forms.Button btnmultipliacar;
+        private System.Windows.Forms.Button btndividir;
+        private System.Windows.Forms.Button btnsumar;
+        private System.Windows.Forms.Button btnrestar;
+        private System.Windows.Forms.Label lbltotal;
     }
 }
 
--- holamundo/Form1.cs
+++ holamundo/Form1.cs
@@ -21,5 +21,54 @@ namespace holamundo
         {
             MessageBox.Show("hola" + txtnombre.Text);
         }
+
+        private void btnsumar_Click(object sender, EventArgs e)
+        {
+            string valor1 = txtnumero1.Text; 
+            string valor2 = txtnumero2.Text;
+            int numero1 = int.Parse(valor1);
+            int numero2 = int.Parse(valor2);
+            int total = numero1 + numero2;
+            lbltotal.Text = total.ToString();
+
+        }
+
+        private void btnrestar_Click(object sender, EventArgs e)
+        {
+            string valor1 = txtnumero1.Text;
+            string valor2 = txtnumero2.Text;
+            int numero1 = int.Parse(valor1);
+            int numero2 = int.Parse(valor2);
+            int total = numero1 - numero2;
+            lbltotal.Text = total.ToString();
+
+        }
+
+        private void btnmultipliacar_Click(object sender, EventArgs e)
+        {
+            string valor1 = txtnumero1.Text;
+            string valor2 = txtnumero2.Text;
+            int numero1 = int.Parse(valor1);
+            int numero2 = int.Parse(valor2);
+            int total = numero1 * numero2;
+            lbltotal.Text = total.ToString();
+
+        }
+
+        private void btndividir_Click(object sender, EventArgs e)
+        {
+            string valor1 = txtnumero1.Text;
+            string valor2 = txtnumero2.Text;
+            int numero1 = int.Parse(valor1);
+            int numero2 = int.Parse(valor2);
+            int total = numero1 / numero2;
+            lbltotal.Text = total.ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
